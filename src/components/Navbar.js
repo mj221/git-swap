30c6ceebbb5fd114102485f2b4e6339f0edd6b80
MJ LEE <mj1201276@yahoo.com.au>
2022-02-12 10:41:09 +1000

    Fixed minor compile warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href=""
+            href="."
             // rel="noopener noreferrer"
           >
             <img src={gitLogo} height='42' alt="" className="mb-1 mr-2"/>
